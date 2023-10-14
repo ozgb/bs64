2706c542ea65e6f504fe5f1e0e5d31607c0b4409
Oscar Bailey <oscarbailey@thorncontractors.co.uk>
2023-10-14 16:32:33 +0100

    feat: remove all LUTs from simple simd implementation

--- src/codecs/safesimd/mod.rs
+++ src/codecs/safesimd/mod.rs
@@ -35,6 +35,26 @@ fn encode_32_inner(src: &[u8], dest: &mut [u8]) -> usize {
 }
 
 #[inline(always)]
+fn encode_any_inner(src: &[InputBytes], dest: &mut [OutputBytes]) -> usize {
+    for (dest, src) in dest.iter_mut().zip(src.iter()) {
+        let n: u32 = ((src.t1 as u32) << 16) + ((src.t2 as u32) << 8) + src.t3 as u32;
+        let n_split = [
+            (n >> 18) & 0x3f,
+            (n >> 12) & 0x3f,
+            (n >> 6) & 0x3f,
+            n & 0x3f,
+        ];
+
+        dest.d1 = CHARS[n_split[0] as usize];
+        dest.d2 = CHARS[n_split[1] as usize];
+        dest.d3 = CHARS[n_split[2] as usize];
+        dest.d4 = CHARS[n_split[3] as usize];
+    }
+
+    dest.len() * 4
+}
+
+#[inline(always)]
 fn encode_32_inner_alt(src: &[InputBytes], dest: &mut [OutputBytes]) -> usize {
     for (dest, src) in dest.iter_mut().zip(src.iter()) {
         let n: u32 = ((src.t1 as u32) << 16) + ((src.t2 as u32) << 8) + src.t3 as u32;
@@ -51,7 +71,7 @@ fn encode_32_inner_alt(src: &[InputBytes], dest: &mut [OutputBytes]) -> usize {
         dest.d4 = CHARS[n_split[3] as usize];
     }
 
-    src.len() * 4
+    32
 }
 
 #[target_feature(enable = "avx")]
@@ -76,14 +96,85 @@ pub unsafe fn encode_32_avx2(src: &[u8], dest: &mut [u8]) -> usize {
     }
 }
 
-pub fn encode_32(src: &[u8], dest: &mut [u8]) -> usize {
-    unsafe {
-        let src: &[InputBytes] = std::slice::from_raw_parts(src.as_ptr() as *const InputBytes, 8);
-        let dest: &mut [OutputBytes] =
-            std::slice::from_raw_parts_mut(dest.as_mut_ptr() as *mut OutputBytes, 8);
+/// Encode 24 bytes from src slice to 32 destination slice
+/// Unsafe because no checks are performed on the slice lengths
+pub unsafe fn encode_32(src: &[u8], dest: &mut [u8]) -> usize {
+    let src: &[InputBytes] = std::slice::from_raw_parts(src.as_ptr() as *const InputBytes, 8);
+    let dest: &mut [OutputBytes] =
+        std::slice::from_raw_parts_mut(dest.as_mut_ptr() as *mut OutputBytes, 8);
 
-        encode_32_inner_alt(src, dest)
+    encode_32_inner_alt(src, dest)
+}
+
+/// Encode any length src and destination slice
+/// Unsafe because no checks are performed on the destination slice length
+unsafe fn encode_any(src: &[u8], dest: &mut [u8]) -> usize {
+    let src_len = src.len() / 3;
+    let src: &[InputBytes] = std::slice::from_raw_parts(src.as_ptr() as *const InputBytes, src_len);
+    let dest: &mut [OutputBytes] =
+        std::slice::from_raw_parts_mut(dest.as_mut_ptr() as *mut OutputBytes, src_len);
+
+    encode_any_inner(src, dest)
+}
+
+pub fn encode_alt(src: &[u8], dest: &mut [u8]) -> usize {
+    let len = src.len();
+    let mut src_i = 0;
+    let mut dest_i = 0;
+    loop {
+        if len - src_i < 24 {
+            break;
+        }
+        unsafe {
+            encode_32(&src[src_i..src_i + 24], &mut dest[dest_i..dest_i + 32]);
+        }
+
+        src_i += 24;
+        dest_i += 32;
     }
+
+    if len - src_i > 2 {
+        unsafe { dest_i += encode_any(&src[src_i..], &mut dest[dest_i..]) }
+        src_i = (dest_i / 4) * 3;
+    }
+
+    match len - src_i {
+        0 => (),
+        1 => {
+            let t1 = src[src_i as usize];
+            let n: u32 = (t1 as u32) << 16;
+            let n_split = [
+                ((n >> 18) & 0x3f) as usize,
+                ((n >> 12) & 0x3f) as usize,
+                ((n >> 6) & 0x3f) as usize,
+                (n & 0x3f) as usize,
+            ];
+
+            dest[dest_i] = CHARS[n_split[0]];
+            dest[dest_i + 1] = CHARS[n_split[1]];
+            dest[dest_i + 2] = CHARPAD;
+            dest[dest_i + 3] = CHARPAD;
+            dest_i += 4;
+        }
+        _ => {
+            let (t1, t2) = (src[src_i], src[src_i + 1]);
+            let n: u32 = ((t1 as u32) << 16) + ((t2 as u32) << 8);
+            let n_split = [
+                ((n >> 18) & 0x3f) as usize,
+                ((n >> 12) & 0x3f) as usize,
+                ((n >> 6) & 0x3f) as usize,
+                (n & 0x3f) as usize,
+            ];
+
+            dest[dest_i] = CHARS[n_split[0]];
+            dest[dest_i + 1] = CHARS[n_split[1]];
+            dest[dest_i + 2] = CHARS[n_split[2]];
+            dest[dest_i + 3] = CHARPAD;
+            dest_i += 4;
+        }
+    }
+
+    dest_i
 }
 
 pub fn encode(src: &[u8], dest: &mut [u8]) -> usize {
@@ -94,7 +185,9 @@ pub fn encode(src: &[u8], dest: &mut [u8]) -> usize {
         if len - src_i < 24 {
             break;
         }
-        encode_32(&src[src_i..src_i + 24], &mut dest[dest_i..dest_i + 32]);
+        unsafe {
+            encode_32(&src[src_i..src_i + 24], &mut dest[dest_i..dest_i + 32]);
+        }
 
         src_i += 24;
         dest_i += 32;
--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,25 @@ fn eval<T: Codec>(c: &T) {
 }
 
 fn eval_chromium() {
-    let num_bytes = 100;
-    let mut output = vec![0u8; (num_bytes * 4) / 3 + 4];
-    let mut bytes = Vec::with_capacity(num_bytes);
-    for i in 0..num_bytes {
-        bytes.push(i as u8);
+    for num_bytes in 100..104 {
+        let mut output = vec![0u8; (num_bytes * 4) / 3 + 4];
+        let mut bytes = Vec::with_capacity(num_bytes);
+        for i in 0..num_bytes {
+            bytes.push(i as u8);
+        }
+        bs64::codecs::safesimd::encode(&bytes, output.as_mut_slice());
+        println!("{:?}", String::from_utf8(output.to_vec()).unwrap());
+        output.fill_with(|| 0);
+        bs64::codecs::safesimd::encode_alt(&bytes, output.as_mut_slice());
+        println!("{:?}", String::from_utf8(output.to_vec()).unwrap());
+        println!("{}", BASE64.encode(&bytes));
     }
-    bs64::codecs::safesimd::encode(&bytes, output.as_mut_slice());
-    println!("{:?}", String::from_utf8(output.to_vec()).unwrap());
-    println!("{}", BASE64.encode(&bytes));
 }
 
 fn print_performance(name: &str, time: Duration, iterations: usize) {
     let its_per_sec = iterations as f64 / time.as_secs_f64();
     let ns_per_it = time.as_nanos() / iterations as u128;
-    println!("{name: <10} | {its_per_sec: <15.2} | {ns_per_it: <10}");
+    println!("{name: <15} | {its_per_sec: <15.2} | {ns_per_it: <10}");
 }
 
 /// main function
@@ -51,7 +55,7 @@ fn main() {
         bytes.push(i as u8);
     }
     println!(
-        "{0: <10} | {1: <15} | {2: <10}",
+        "{0: <15} | {1: <15} | {2: <10}",
         "name", "its_per_sec", "ns_per_it"
     );
 
@@ -79,6 +83,14 @@ fn main() {
     }
     let total = start.elapsed();
     print_performance("safesimd", total, iterations);
+
+    let mut output = vec![0u8; (num_bytes * 4) / 3 + 4];
+    let start = Instant::now();
+    for _ in 0..iterations {
+        bs64::codecs::safesimd::encode_alt(&bytes, output.as_mut_slice());
+    }
+    let total = start.elapsed();
+    print_performance("safesimd_alt", total, iterations);
 
     //let start = Instant::now();
     //for _ in 0..iterations {

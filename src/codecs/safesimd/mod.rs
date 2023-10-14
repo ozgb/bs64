e759caf4e09d751596bb0526bd96d4772fe8285e
Oscar Bailey <oscarbailey@thorncontractors.co.uk>
2023-10-14 12:48:58 +0100

    refactor: cleanup chromium rust implementation

--- src/codecs/safesimd/mod.rs
+++ src/codecs/safesimd/mod.rs
@@ -25,17 +25,12 @@ pub fn encode(src: &[u8], dest: &mut [u8]) -> usize {
 
     if len - src_i > 2 {
         for i in (src_i..len - 2).step_by(3) {
-            t1 = src[i];
-            t2 = src[i + 1];
-            t3 = src[i + 2];
+            (t1, t2, t3) = (src[src_i], src[src_i + 1], src[src_i + 2]);
             dest[dest_i] = E0[t1 as usize];
-            dest_i += 1;
-            dest[dest_i] = E1[(((t1 & 0x03) << 4) | ((t2 >> 4) & 0x0F)) as usize];
-            dest_i += 1;
-            dest[dest_i] = E1[(((t2 & 0x0F) << 2) | ((t3 >> 6) & 0x03)) as usize];
-            dest_i += 1;
-            dest[dest_i] = E2[t3 as usize];
-            dest_i += 1;
+            dest[dest_i + 1] = E1[(((t1 & 0x03) << 4) | ((t2 >> 4) & 0x0F)) as usize];
+            dest[dest_i + 2] = E1[(((t2 & 0x0F) << 2) | ((t3 >> 6) & 0x03)) as usize];
+            dest[dest_i + 3] = E2[t3 as usize];
+            dest_i += 4;
             src_i = i + 3;
         }
     }
@@ -45,25 +40,18 @@ pub fn encode(src: &[u8], dest: &mut [u8]) -> usize {
         1 => {
             t1 = src[src_i as usize];
             dest[dest_i] = E0[t1 as usize];
-            dest_i += 1;
-            dest[dest_i] = E1[((t1 & 0x03) << 4) as usize];
-            dest_i += 1;
-            dest[dest_i] = CHARPAD;
-            dest_i += 1;
-            dest[dest_i] = CHARPAD;
-            dest_i += 1;
+            dest[dest_i + 1] = E1[((t1 & 0x03) << 4) as usize];
+            dest[dest_i + 2] = CHARPAD;
+            dest[dest_i + 3] = CHARPAD;
+            dest_i += 4;
         }
         _ => {
-            t1 = src[src_i];
-            t2 = src[src_i + 1];
+            (t1, t2) = (src[src_i], src[src_i + 1]);
             dest[dest_i] = E0[t1 as usize];
-            dest_i += 1;
-            dest[dest_i] = E1[(((t1 & 0x03) << 4) | ((t2 >> 4) & 0x0F)) as usize];
-            dest_i += 1;
-            dest[dest_i] = E2[((t2 & 0x0F) << 2) as usize];
-            dest_i += 1;
-            dest[dest_i] = CHARPAD;
-            dest_i += 1;
+            dest[dest_i + 1] = E1[(((t1 & 0x03) << 4) | ((t2 >> 4) & 0x0F)) as usize];
+            dest[dest_i + 2] = E2[((t2 & 0x0F) << 2) as usize];
+            dest[dest_i + 3] = CHARPAD;
+            dest_i += 4;
         }
     }
 
